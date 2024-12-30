ae6d4f088858a45f564b682d8aad63a62258b934
Alexis Reubrecht <lxsrbrcht@gmail.com>
2024-12-30 03:26:29 +0100

    campaigns implemented, needs styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_29_184154) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_29_193544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "campaigns", force: :cascade do |t|
+    t.string "title"
+    t.text "synopsis"
+    t.string "progress"
+    t.string "format"
+    t.string "setting"
+    t.json "players"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_campaigns_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +40,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_29_184154) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "campaigns", "users"
 end
